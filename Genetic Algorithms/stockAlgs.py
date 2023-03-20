0d7d2b3428ed9179e789631c7d0a9d3f92c6b7c6
Garrett Powell <garettbpowell@gmail.com>
2023-03-20 18:21:28 -0500

    should correctly buy throughout the file now

--- Genetic Algorithms/stockAlgs.py
+++ Genetic Algorithms/stockAlgs.py
@@ -8,28 +8,6 @@ rnd = np.random.RandomState(RANDOM_SEED_VALUE)
 def calcSMA(dataRange):
     return(sum(dataRange) / len(dataRange))
         
-# returns true if current day is greater than the simple moving average of n days, returns false if n > len of file data
-def simpleMovingAverage(stockData, N):
-    index = 0
-    currentRange = []
-    trueIndexes = set({})
-    length = len(stockData) - 1
-    if(len(stockData) <= N):
-        return trueIndexes
-
-    while(index < length):
-        while(index < length and len(currentRange) < N):
-            currentRange.append(stockData[index])
-            index += 1
-        if(index >= length):
-            return trueIndexes
-
-        if(stockData[index + 1] > calcSMA(currentRange)):
-           trueIndexes.add(index + 1)
-
-        del(currentRange[0])
-
-    return trueIndexes
 
 def calcEMA(dataRange):
     a = 2 / (len(dataRange) + 1)
@@ -41,31 +19,7 @@ def calcEMA(dataRange):
 
     return totalSum / totalDenom
 
-# returns true if current day is greater than the exponential moving average of n days, returns false if n > len of file data
-def exponentialMovingAverage(stockData, N):
-    index = 0
-    currentRange = []
-    trueIndexes = set({})
-    length = len(stockData) - 1
-
-    if(len(stockData) <= N):
-        return trueIndexes
-
-    while(index < length):
-        while(index < length and len(currentRange) < N):
-            currentRange.append(stockData[index])
-            index += 1
-        if(index >= length):
-            return trueIndexes
-
-        if(stockData[index + 1] > calcEMA(currentRange)):
-           trueIndexes.add(index + 1)
-
-        del(currentRange[0])
-
-    return trueIndexes
-
-def CalcMax(dataRange):
+def calcMax(dataRange):
     return max(dataRange)
 
 # sees if current day is greater than max of previous n days, returns false if n > len of file data
@@ -91,98 +45,134 @@ def maximum(stockData, N):
 
     return trueIndexes
 
+# driver that returns the correct rule's value
+def runRule(rule, range):
+    if rule[0] == 's':
+        return calcSMA(range)
+    elif rule[0] == 'e':
+        return calcEMA(range)
+    else:
+        return calcMax(range)
+
+    return -1
+
+
 # runs input data against 3 rules contained in genotype
 def calcFitness(stockData, population):
     fitPop = []
     for genotypes in population:
         # somehow break out the different rules
         ruleBlock = genotypes[1]
-        rules = [(set({}), ruleBlock[0], int(ruleBlock[1:4]), ruleBlock[4]), (set({}), ruleBlock[5], int(ruleBlock[6:9]), ruleBlock[9]), (set({}), ruleBlock[10], int(ruleBlock[11:14]), 'end')]
+        rules = [(ruleBlock[0], int(ruleBlock[1:4]), ruleBlock[4]), (ruleBlock[5], int(ruleBlock[6:9]), ruleBlock[9]), (ruleBlock[10], int(ruleBlock[11:14]), 'end')]
 
         profit = 0
         availableFunds = 20000
         response = set({})
+        numOfPurchasedStocks = 0
 
-        for dataset in stockData:    
-            if(availableFunds > 0):
-                newRules = []
+        if not(rules[0][1] == 0 and rules[1][1] == 0 and rules[2][1] == 0):
+            for dataset in stockData:
 
-                # if the rules are ands, change the index that it starts at to exclude stuff that does not need to be calculated
-                for rule in rules:
-                    if(rule[2] != 0):
-                        if(rule[1] == 's'):
-                            newRules.append((simpleMovingAverage(dataset, rule[2]), rule[1], rule[2], rule[3]))
-                        elif(rule[1] == 'e'):
-                            newRules.append((exponentialMovingAverage(dataset, rule[2]), rule[1], rule[2], rule[3]))
-                        elif(rule[1] == 'm'):
-                            newRules.append((maximum(dataset, rule[2]), rule[1], rule[2], rule[3]))
-                    else:
-                        newRules.append((set({}), rule[1], rule[2], rule[3]))
+                rule0Range = []
+                rule1Range = []
+                rule2Range = []
 
-                # if all rules are & start first rule and run entire sequence, take second rule and start it at the index where the first rule is first true. Take the last rule and start
-                # it at the first index of rule 1 and 2 where it is true, else don't run the rule
+                for data in dataset:
+                    ruleBoolean = [False, False, False]
 
-                # if first rule is & run the entire first rule and then start the second rule from the first true index of the first rule. Run 3rd rule up to index unless the index of the first union is less than the rule
-                
-                
-                # if all rules are & then get the intersection
-                if(newRules[0][3] == '&' and newRules[1][3] == '&'):
-                    if(newRules[0][2] != 0):
-                        response = newRules[0][0]
-                        if(newRules[1][2] != 0):
-                            response = response.intersection(newRules[1][0])
-                        if(newRules[2][2] != 0):
-                            response = response.intersection(newRules[2][0])
-                    elif(newRules[1][2] != 0):
-                        response = newRules[1][0]
-                        if(newRules[2][2] != 0):
-                            response = response.intersection(newRules[2][0])
-                    else:
-                        response = newRules[2][0]
+                    # adjust profit and available funds to be appropriate starting ammounts
+                    if(availableFunds > 20000):
+                        profit += availableFunds - 20000
+                        availableFunds = 20000
+                    elif(availableFunds < 20000 and numOfPurchasedStocks == 0):
+                        if(profit - (20000 - availableFunds) > 0):
+                            profit -= 20000 - availableFunds
+                            availableFunds = 20000
+                        else:
+                            availableFunds += profit
+                            profit = 0
 
-                # if first rule is & get the intersection of the first two
-                elif(newRules[0][3] == '&'):
-                     if(newRules[0][2] != 0):
-                        response = newRules[0][0]
-                        if(newRules[1][2] != 0):
-                            response = response.intersection(newRules[1][0])
-                     response = response.union(newRules[2][0])
+                # add current data to previous data and adjust accordingly
+                    if( len(rule0Range) < rules[0][1]):
+                        rule0Range.append(data)
+                        ruleBoolean[0] = False
+                    elif(rules[0][1] != 0) :                 
+                        ruleReturn = runRule(rules[0], rule0Range)
+                        if(numOfPurchasedStocks == 0 and data > ruleReturn):
+                            ruleBoolean[0] = True
+                        elif numOfPurchasedStocks > 0 and data < ruleReturn:
+                            ruleBoolean[0] = True
+               
+                        del(rule0Range[0])
+                        rule0Range.append(data)
 
-                # if second rule is & get the intersection of the second two
-                elif(newRules[1][3] == '&'):
-                     if(newRules[1][2] != 0):
-                        response = newRules[1][0]
-                        if(newRules[2][2] != 0):
-                            response = response.intersection(newRules[2][0])
-                     else:
-                         response = newRules[2][0]
-                     response = response.union(newRules[0][0])
-                else:
-                    response = (newRules[0][0].union(newRules[1][0])).union(newRules[2][0])
+                    if( len(rule1Range) < rules[1][1]):
+                       rule1Range.append(data)
+                       ruleBoolean[1] = False
+                    elif rules[1][1] != 0:
+                        ruleReturn = runRule(rules[1], rule1Range)
+                        if(numOfPurchasedStocks == 0 and data > ruleReturn):
+                            ruleBoolean[1] = True
+                        elif numOfPurchasedStocks > 0 and data < ruleReturn:
+                            ruleBoolean[1] = True
+
+                        del(rule1Range[0])
+                        rule1Range.append(data)
+
+                    if( len(rule2Range) < rules[2][1]):
+                       rule2Range.append(data)
+                       ruleBoolean[2] = False
+                    elif rules[2][1] != 0:
+                        ruleReturn = runRule(rules[2], rule2Range)
+                        if(numOfPurchasedStocks == 0 and data > ruleReturn):
+                            ruleBoolean[2] = True
+                        elif numOfPurchasedStocks > 0 and data < ruleReturn:
+                            ruleBoolean[2] = True
+
+                        del(rule2Range[0])
+                        rule2Range.append(data)
 
 
+                    # determine if the rule is true
+                    choice = False
 
-                #print("Available Funds: ", availableFunds, " Profit: ", profit, " Response: ", response)
-                # buy and sell stocks
-                if(len(response) > 0):
-                    totalStock = availableFunds / response.pop()
-                    availableFunds = (dataset[-1]) * totalStock
-                # didn't buy 
-                else:
-                    availableFunds = availableFunds / 2
-            
-                # move funds to correct place 
-                if(availableFunds > 20000):
-                    profit += availableFunds - 20000
-                    availableFunds -= (availableFunds - 20000)
-                elif(profit > 20000 - availableFunds):
-                    profit -= 20000 - availableFunds
-                    availableFunds = 20000
-                else:
-                    availableFunds += profit
-                    profit = 0
+                    # check that the range is not 0 meaning that that portion of the rule should be excluded
+                    # if rules are applied it checks the bools determined from the above code against each portion of the rule from left to right
+                    # if 2 of the 3 rules are excluded it takes the bool of the remaining rule
+                    # if the middle rule is excluded it takes the bool of the first and last rule and uses the 2 operator in the sequence ie. s100|m000&s500 would do s100&s500
+                    if rules[0][1] != 0 and rules[1][1] != 0:
+                        if(rules[0][2] == '&'):
+                            choice = ruleBoolean[0] and ruleBoolean[1]
+                        else:
+                            choice = ruleBoolean[0] or ruleBoolean[1]
+                    elif rules[0][1] != 0:
+                        choice = ruleBoolean[0]
+                    elif rules[1][1] != 0:
+                        choice = ruleBoolean[1]
+
+                    if rules[2][1] != 0 and rules[1][1] != 0:
+                        if rules[1][2] == '&':
+                            choice = choice and ruleBoolean[2]
+                        else:
+                            choice = choice or ruleBoolean[2]
+                    elif rules[2][1] != 0 and rules[0][1] != 0:
+                        if rules[1][2] == '&':
+                            choice = choice and ruleBoolean[2]
+                        else:
+                            choice = choice or ruleBoolean[2]
+                    elif rules[2][1] != 0:
+                        choice = ruleBoolean[2]
+
+                    if(choice):
+                        if(numOfPurchasedStocks == 0):
+                            numOfPurchasedStocks = availableFunds / data
+                            availableFunds = 0
+                        else:
+                            availableFunds = numOfPurchasedStocks * data
+                            numOfPurchasedStocks = 0
+
         # save fitness
-        fitPop.append((profit + availableFunds, genotypes[1]))
+        fitPop.append((profit, genotypes[1]))
     return fitPop
 
 def generateIntermediatePopulation():
